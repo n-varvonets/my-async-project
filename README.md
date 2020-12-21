f28fa8b420e8e86cdbbd7f60b6ee07ba4d562e57
n-varvonets <nickolay.varvonets@gmail.com>
2020-12-21 14:10:18 +0200

    added allure and fill README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# my-async-project
+# my-async-project 
+
+1. During project development Python 3.6 and the operating system Windows 10 were used.<br>
+2. Run this command to install requirements:\
+<b>pip install -r requirements.txt</b>
+
+3. To run the server:\
+<b>python server.py</b>
+
+4. To run the client:\
+<b>python client.py</b>  
+5. To check allure report make sure you have <a href="https://docs.qameta.io/allure/#_installing_a_commandline">allure</a> installed.\
+Generate allure report:\
+<b>python -m pytest --alluredir results  tests/ </b>\
+Run allure to watch report:\
+<b>allure serve ./results </b>
+
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from aiohttp import web
 
@@ -28,33 +29,57 @@ def cli(loop, aiohttp_client):
     return loop.run_until_complete(aiohttp_client(app))
 
 
+@allure.feature('Method "process_pokemon_handler"')
+@allure.story('Handler works correct')
+@allure.severity("blocker")
 async def test_process_pokemon_handler(cli):
     resp = await cli.post('/', data=MOCK_POKEMON_REQUEST_DATA)
-    assert resp.status == 200
-    assert await resp.text() == '{"name": "pidgeot_the_boss", "id": 18}'
+
+    with allure.step("Check status code"):
+        assert resp.status == 200
+    with allure.step("Check text response"):
+        assert await resp.text() == '{"name": "pidgeot_the_boss", "id": 18}'
 
 
+@allure.feature('Method "process_pokemon_handler"')
+@allure.story('Handler return BAD REQUEST')
+@allure.severity("blocker")
 async def test_process_pokemon_handler_bad_request(cli):
     resp = await cli.post('/', data=MOCK_POKEMON_REQUEST_DATA_BAD_REQUEST)
-    assert resp.status == 400
-    assert await resp.text() == 'Missed required field: "id"'
+
+    with allure.step("Check status code"):
+        assert resp.status == 400
+    with allure.step("Check text response"):
+        assert await resp.text() == 'Missed required field: "id"'
 
 
+@allure.feature('Name changing')
+@allure.story('Name change for a boss pokemon')
+@allure.severity("critical")
 def test_change_pokemon_name_boss():
     pokemon = {"id": 18, "name": "pidgeot", "weight": 395}
     assert change_pokemon_name(pokemon) == "pidgeot_the_boss"
 
 
+@allure.feature('Name changing')
+@allure.story('Name change for a little pokemon')
+@allure.severity("critical")
 def test_change_pokemon_name_feather():
     pokemon = {"id": 18, "name": "pidgeot", "weight": 40}
     assert change_pokemon_name(pokemon) == "like_a_feather_pidgeot"
 
 
+@allure.feature('Name changing')
+@allure.story('Name does not change')
+@allure.severity("critical")
 def test_change_pokemon_name_normal():
     pokemon = {"id": 18, "name": "pidgeot", "weight": 51}
     assert change_pokemon_name(pokemon) == "pidgeot"
 
 
+@allure.feature('Checking for required fields')
+@allure.story('All required fields are present')
+@allure.severity("critical")
 def test_check_required_field():
     pokemon = {"id": 18, "name": "pidgeot", "weight": 395}
     valid, field = check_required_field(pokemon)
@@ -62,6 +87,9 @@ def test_check_required_field():
     assert field is None
 
 
+@allure.feature('Checking for required fields')
+@allure.story('Missing field "name"')
+@allure.severity("critical")
 def test_check_required_field_no_name():
     pokemon = {"id": 18, "weight": 395}
     valid, field = check_required_field(pokemon)
@@ -69,6 +97,9 @@ def test_check_required_field_no_name():
     assert field == "name"
 
 
+@allure.feature('Checking for required fields')
+@allure.story('Missing field "id"')
+@allure.severity("critical")
 def test_check_required_field_no_id():
     pokemon = {"name": "pidgeot", "weight": 395}
     valid, field = check_required_field(pokemon)
